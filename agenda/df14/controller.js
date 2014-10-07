c3275c346fdb0ff6eaf38cc25e65c16052c1efa4
Patrick Connelly <patrick@deadlypenguin.com>
2014-10-07 16:06:23 -0400

    adding df14 agenda

---
+++ agenda/df14/controller.js
@@ -0,0 +1,15 @@
+/*jslint browser: true, regexp: true */
+/*global angular */
+
+var ccmApp = angular.module('agendaApp', []);
+
+ccmApp.controller('AgendaCtrl', ['$scope', '$http', function ($scope, $http) {
+	'use strict';
+	$scope.loading = true;
+
+	$http.get('schedule.json').success(function (data) {
+		console.log(data);
+		$scope.data = data;
+		$scope.loading = false;
+	});
+}]);
